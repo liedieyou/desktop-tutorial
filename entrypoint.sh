ae72c03315a06d7b6c76aca74b35e9fc03a19834
liedieyou <76842456+liedieyou@users.noreply.github.com>
2021-09-13 23:51:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mkdir ~/xray_cert
+mkdir -p /xray_cert
 acme.sh  --register-account  -m myemail@trytrysslssl.cf --server zerossl
 acme.sh --issue -d trytrysslssl.cf --standalone --httpport 443
 acme.sh --install-cert -d trytrysslssl.cf \
---key-file       ~/xray_cert/xray.crt  \
---fullchain-file ~/xray_cert/xray.key 
+--key-file       /xray_cert/xray.crt  \
+--fullchain-file /xray_cert/xray.key 
