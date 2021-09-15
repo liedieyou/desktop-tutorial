3008ceb280fd4892881e812e36ffc1a955f0a1de
liedieyou <76842456+liedieyou@users.noreply.github.com>
2021-09-15 20:04:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p /xray_cert
-acme.sh --issue -d acmetry.herokuapp.com --alpn --server letsencrypt
+acme.sh --issue -d acmetry.herokuapp.com --alpn --server letsencrypt --debug 2
 acme.sh --install-cert -d trytrysslssl.cf \
 --key-file       /xray_cert/xray.crt  \
 --fullchain-file /xray_cert/xray.key 
