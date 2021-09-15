ba425b3598a82fadac33b294f1435db08cc1e6f7
liedieyou <76842456+liedieyou@users.noreply.github.com>
2021-09-15 23:28:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/acmesh-official/acme.sh/archive/refs/tags/3.0.0.zip -o /etc/3.0.0.zip
 busybox unzip /etc/3.0.0.zip -d /
 
-mkdir -p /xray_cert
+
 acme.sh --issue -d acmetry.herokuapp.com --alpn --server letsencrypt --debug 2 --log  "/mylog.log"
 
 acme.sh --install-cert -d trytrysslssl.cf \
